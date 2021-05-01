d408e3e137e9b9aa57b73747cfdaa5d4e349863d
DESKTOP-8GMDJIR\EunBKo <minniemice98@gmail.com>
2021-05-01 15:38:07 +0900

    2021-05-1 15:30

---
+++ oop/calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+
+    def __init__(self, first, second ):
+        self.first = first
+
+    def sum(self):
+        pass
+
+    def sum(self):
+        pass
+
+    def mul(self):
+        pass
+
+    def div(self):
+        pass
+
+
+
